6c2bcefcc777c65fa269e3faa36b52733d8ff186
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-12-07 01:58:25 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+soilMoisturePins = [34, 35]
+dhtPin = 32
+motorDriverPins = [33, 25, 26, 27]
+waterLevelPin = 14
+i2cPins = [21, 22]
+
+ap_ssid = "SGP-WiFi"
+ap_password = "Kelompok11"
+ap_authmode = 3
+
+firebase_url = "https://green-house-monitoring-65fe4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+path_version = "v1"
